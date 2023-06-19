545a7e62a02c5fc5e295f71dc3a1e1f4c543e63e
antikeya <anitkleya@yandex.ru>
2023-06-19 23:04:48 +0300

    renamed token constant

--- api/app/auth/manager.py
+++ api/app/auth/manager.py
@@ -5,12 +5,12 @@ from fastapi_users import BaseUserManager, IntegerIDMixin
 
 from app.auth.models import User
 from app.auth.services import get_user_db
-from app.config import PASSWORD_VERIFY_TOKEN
+from app.config import PASSWORD_VERIFICATION_TOKEN
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = PASSWORD_VERIFY_TOKEN
-    verification_token_secret = PASSWORD_VERIFY_TOKEN
+    reset_password_token_secret = PASSWORD_VERIFICATION_TOKEN
+    verification_token_secret = PASSWORD_VERIFICATION_TOKEN
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
